9fc2903d9f471daa21dd15ef0d7baa223d8e3fe0
Shayan Saeed <115692639+shayansaeed123@users.noreply.github.com>
2024-02-02 15:07:49 +0000

    account
    account

--- lib/repo/widgets/text_field.dart
+++ lib/repo/widgets/text_field.dart
@@ -2,16 +2,27 @@ import 'package:banking/repo/colors.dart';
 import 'package:flutter/material.dart';
 
 class TextFiledWidget extends StatelessWidget {
-  TextFiledWidget({super.key,required this.controller,required this.textHint,required this.textAlign,required this.textInputType, required this.obscureChara,required this.obscure});
+  TextFiledWidget({super.key,
+    required this.controller,
+    required this.textHint,
+    required this.textAlign,
+    required this.textInputType,
+    required this.obscureChara,
+    required this.obscure,
+    this.border =10,
+  });
   TextEditingController controller = TextEditingController();
   TextInputType textInputType = TextInputType.none;
   bool obscure = true;
   String obscureChara = '';
   String textHint = '';
   TextAlign textAlign;
+  double border;
+
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width * 1;
+    final height = MediaQuery.of(context).size.height * 1;
     return Container(
       width: width * 0.8,
       child: TextFormField(
@@ -21,10 +32,11 @@ class TextFiledWidget extends StatelessWidget {
         obscureText: obscure,
         obscuringCharacter: obscureChara,
         decoration: InputDecoration(
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10),borderSide: BorderSide(width: 2)),
-          enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10),borderSide: BorderSide(color: AppColors.borderColor,width: 2)),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(border),borderSide: BorderSide(width: 2)),
+          enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(border),borderSide: BorderSide(color: AppColors.borderColor,width: 2)),
           hintText: textHint,
-          focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10),borderSide: BorderSide(color: AppColors.enableBorder,width: 2)),
+          contentPadding: EdgeInsets.only(left: width*0.02,top: height*0.0),
+          focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(border),borderSide: BorderSide(color: AppColors.enableBorder,width: 2)),
         ),
       ),
     );
--- lib/views/account_screen.dart
+++ lib/views/account_screen.dart
@@ -1,3 +1,4 @@
+import 'package:banking/repo/widgets/text_field.dart';
 import 'package:flutter/material.dart';
 
 import '../repo/colors.dart';
@@ -10,6 +11,12 @@ class AccountScreen extends StatefulWidget {
 }
 
 class _AccountScreenState extends State<AccountScreen> {
+  final name = TextEditingController();
+  final account = TextEditingController();
+  final email = TextEditingController();
+  final password = TextEditingController();
+  final phone = TextEditingController();
+  final address = TextEditingController();
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width * 1;
@@ -35,6 +42,19 @@ class _AccountScreenState extends State<AccountScreen> {
           children: [
             SizedBox(height: height*0.02,),
             Icon(Icons.person_pin,color: AppColors.iconColor2,size: 100,),
+            SizedBox(height: height*0.02,),
+            Container(
+              height: height*0.04,
+              child: TextFiledWidget(
+                controller: email,
+                textHint: 'Your Name',
+                textAlign: TextAlign.start,
+                textInputType: TextInputType.text,
+                obscureChara: '*',
+                obscure: false,
+                border: 4,
+              ),
+            )
           ],
         ),
       ),
