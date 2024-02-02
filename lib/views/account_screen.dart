06c9a803a9d6600281c1c6660fdd4aa97254fbf0
Shayan Saeed <115692639+shayansaeed123@users.noreply.github.com>
2024-02-02 15:20:51 +0000

    new update
    new update

--- lib/views/account_screen.dart
+++ lib/views/account_screen.dart
@@ -1,3 +1,4 @@
+import 'package:banking/repo/widgets/button.dart';
 import 'package:banking/repo/widgets/text_field.dart';
 import 'package:flutter/material.dart';
 
@@ -41,19 +42,95 @@ class _AccountScreenState extends State<AccountScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             SizedBox(height: height*0.02,),
-            Icon(Icons.person_pin,color: AppColors.iconColor2,size: 100,),
-            SizedBox(height: height*0.02,),
+            Icon(Icons.person_pin,color: AppColors.iconColor2,size: 150,),
+            SizedBox(height: height*0.04,),
             Container(
               height: height*0.04,
               child: TextFiledWidget(
-                controller: email,
-                textHint: 'Your Name',
+                controller: name,
+                textHint: 'YOUR NAME',
                 textAlign: TextAlign.start,
                 textInputType: TextInputType.text,
                 obscureChara: '*',
                 obscure: false,
                 border: 4,
               ),
+            ),
+            SizedBox(height: height*0.02,),
+            Container(
+              height: height*0.04,
+              child: TextFiledWidget(
+                controller: account,
+                textHint: 'BANK ACCOUNT',
+                textAlign: TextAlign.start,
+                textInputType: TextInputType.text,
+                obscureChara: '*',
+                obscure: false,
+                border: 4,
+              ),
+            ),
+            SizedBox(height: height*0.02,),
+            Container(
+              height: height*0.04,
+              child: TextFiledWidget(
+                controller: email,
+                textHint: 'EMAIL',
+                textAlign: TextAlign.start,
+                textInputType: TextInputType.text,
+                obscureChara: '*',
+                obscure: false,
+                border: 4,
+              ),
+            ),
+            SizedBox(height: height*0.02,),
+            Container(
+              height: height*0.04,
+              child: TextFiledWidget(
+                controller: password,
+                textHint: 'PASSWORD',
+                textAlign: TextAlign.start,
+                textInputType: TextInputType.text,
+                obscureChara: '*',
+                obscure: false,
+                border: 4,
+              ),
+            ),
+            SizedBox(height: height*0.02,),
+            Container(
+              height: height*0.04,
+              child: TextFiledWidget(
+                controller: phone,
+                textHint: 'PHONE NUMBER',
+                textAlign: TextAlign.start,
+                textInputType: TextInputType.text,
+                obscureChara: '*',
+                obscure: false,
+                border: 4,
+              ),
+            ),
+            SizedBox(height: height*0.02,),
+            Container(
+            width: width * 0.8,
+            child: TextFormField(
+            controller: address,
+            textAlign: TextAlign.start,
+            keyboardType: TextInputType.text,
+            maxLines: 5,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(4),borderSide: BorderSide(width: 2)),
+              enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(4),borderSide: BorderSide(color: AppColors.borderColor,width: 2)),
+              hintText: 'YOUR ADDRESS',
+              contentPadding: EdgeInsets.only(left: width*0.02,top: height*0.02),
+              focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(4),borderSide: BorderSide(color: AppColors.enableBorder,width: 2)),
+            ),
+          ),
+        ),
+            SizedBox(height: height*0.05,),
+            ButtonWidget(
+              onTap: (){},
+              text: 'SAVE CHANGES',
+              buttonTextColor: AppColors.buttonTextColor,
+              btnWidth: width*0.6,
             )
           ],
         ),
